0e531b9bb8c0c967cc295611d325a46fbd104b1d
KZGYU <adventcdl777@yahoo.co.jp>
2025-03-12 17:47:03 +0900

    Add .gitignore and D256.java

---
+++ src/Dlank/D256.java
@@ -0,0 +1,18 @@
+package Dlank;
+
+import java.util.Scanner;
+public class D256 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        
+        x= x-100;
+        y = y + 100;
+        
+        System.out.println(x);
+		System.out.println(y);
+
+    }
+}
