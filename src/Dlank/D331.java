c5709c0bc82dff78602e0ccc81a2822996cc9427
KZGYU <adventcdl777@yahoo.co.jp>
2025-03-14 15:29:33 +0900

    20250314-2_test

---
+++ src/Dlank/D331.java
@@ -0,0 +1,16 @@
+package Dlank;
+
+import java.util.Scanner;
+
+public class D331 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str1 = sc.nextLine();
+		String str2 = str1.replace(" ", "_");
+		
+		System.out.println(str2);
+		
+		
+	}
+
+}
